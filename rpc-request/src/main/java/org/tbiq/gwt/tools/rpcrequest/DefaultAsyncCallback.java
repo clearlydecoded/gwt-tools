f5f1d0e4bc5cca833fd399df9761dd9173e88e44
yaakov.chaikin <yaakov.chaikin@845de66a-1784-11df-8596-193289140cd3>
2010-02-22 03:45:09 +0000

    Some more updates... just to back up code.

--- rpc-request/src/main/java/org/tbiq/gwt/tools/rpcrequest/DefaultAsyncCallback.java
+++ rpc-request/src/main/java/org/tbiq/gwt/tools/rpcrequest/DefaultAsyncCallback.java
@@ -13,12 +13,38 @@ package org.tbiq.gwt.tools.rpcrequest;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 /**
+ * TODO: JavaDoc
  * 
  * @author Yaakov Chaikin (yaakov.chaikin@gmail.com)
  */
 public abstract class DefaultAsyncCallback<T extends RpcResponse>
   implements AsyncCallback<T>
 {
+  /** Application-wide exception handler. */
+  protected ApplicationWideExceptionHandler exceptionHandler;
+
+  /**
+   * TODO: write javadoc here.
+   * 
+   * @param exception
+   * @return
+   */
+  protected boolean handleException(Throwable exception)
+  {
+    // Check if application wide exception handler is available
+    if (exceptionHandler == null)
+    {
+      // Can't handle exception as app wide exception handler is missing
+      return false;
+    }
+
+    // Use central exception handler to handle this exception
+    exceptionHandler.handleException(exception);
+
+    // Nofify that the exception has been handled
+    return true;
+  }
+
   /*
    * (non-Javadoc)
    * 
@@ -37,9 +63,13 @@ public abstract class DefaultAsyncCallback<T extends RpcResponse>
    * @see com.google.gwt.user.client.rpc.AsyncCallback#onSuccess(java.lang.Object)
    */
   @Override
-  public void onSuccess(T result)
+  public void onSuccess(T response)
   {
-    // TODO Auto-generated method stub
-
+    processResponse(response);
   }
+
+  /**
+   * @param response
+   */
+  protected abstract void processResponse(T response);
 }
