28ad67c6e393104fa3ea66af81b6f0f12fa8b4cb
yaakov.chaikin <yaakov.chaikin@845de66a-1784-11df-8596-193289140cd3>
2010-02-22 18:52:51 +0000

    Copying for creating a presenter module.

---
+++ presenter/src/main/java/org/tbiq/gwt/tools/rpcrequest/RpcRequestService.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2010 Yaakov Chaikin (yaakov.chaikin@gmail.com) Licensed under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed
+ * to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under the
+ * License.
+ */
+package org.tbiq.gwt.tools.rpcrequest;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+/**
+ * RpcRequestService interface defines the single execute method (command pattern) to
+ * carry out the RPC request.
+ * 
+ * @author Yaakov Chaikin (yaakov.chaikin@gmail.com)
+ */
+@RemoteServiceRelativePath("rpc-request")
+public interface RpcRequestService
+  extends RemoteService
+{
+  /**
+   * Executes some {@link RpcRequest} and returns some {@link RpcResponse}. This method
+   * follows Command pattern.
+   * 
+   * @param rpcRequest RPC request object containing data which is needed in order to
+   *          execute the request.
+   * @return Response object which contains data which is the product of executing the RPC
+   *         request.
+   * @throws RpcRequestException If anything goes wrong with executing the RPC request.
+   */
+  public <T extends RpcResponse> T execute(RpcRequest<T> rpcRequest)
+    throws RpcRequestException;
+}
