447069086ddd7d5fedcdab93798228cdb3167bd9
yaakov.chaikin <yaakov.chaikin@845de66a-1784-11df-8596-193289140cd3>
2010-02-23 16:04:58 +0000

    Created dummy RPC request and response classes to satisfy GWT compiler when a new project has no implementations of those interfaces of its own. (See those classes JavaDocs).

---
+++ rpc-request/src/main/java/org/tbiq/gwt/tools/rpcrequest/browser/DummyRpcResponse.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2010 Yaakov Chaikin (yaakov.chaikin@gmail.com) Licensed under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed
+ * to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under the
+ * License.
+ */
+package org.tbiq.gwt.tools.rpcrequest.browser;
+
+/**
+ * DummyRpcResponse class is only here to allow the templated (generics-enabled)
+ * {@link RpcRequestService} to compile in dependent modules. Without it, the dependent
+ * modules would not GWT compile because the GWT compiler would fail to resolve
+ * {@link RpcRequestService} via deferred binding.
+ * <p>
+ * Note that this is only a problem in the initial stages of the project when the new
+ * project has no implementations of {@link RpcRequest} and {@link RpcResponse} of its
+ * own.
+ * 
+ * @author Yaakov Chaikin (yaakov.chaikin@gmail.com)
+ */
+@SuppressWarnings("serial")
+public class DummyRpcResponse
+  implements RpcResponse
+{
+}
