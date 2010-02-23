85b1137e1d52cd070c79c9fbf39f57db2996cb33
yaakov.chaikin <yaakov.chaikin@845de66a-1784-11df-8596-193289140cd3>
2010-02-23 01:50:44 +0000

    Beginning to implement the server side of the RPC Request service.

---
+++ rpc-request/src/main/java/org/tbiq/gwt/tools/rpcrequest/server/RpcRequestServiceServlet.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2010 Yaakov Chaikin (yaakov.chaikin@gmail.com) Licensed under the Apache
+ * License, Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law or agreed
+ * to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under the
+ * License.
+ */
+package org.tbiq.gwt.tools.rpcrequest.server;
+
+import org.tbiq.gwt.tools.rpcrequest.browser.RpcRequest;
+import org.tbiq.gwt.tools.rpcrequest.browser.RpcRequestException;
+import org.tbiq.gwt.tools.rpcrequest.browser.RpcRequestService;
+import org.tbiq.gwt.tools.rpcrequest.browser.RpcResponse;
+
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * RpcRequestServiceServlet class is the server-side part of the {@link RpcRequestService}
+ * interface.
+ * 
+ * @author Yaakov Chaikin (yaakov.chaikin@gmail.com)
+ */
+public class RpcRequestServiceServlet
+  extends RemoteServiceServlet
+  implements RpcRequestService
+{
+  /** Class version ID. */
+  private static final long serialVersionUID = 1296115987581738426L;
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * org.tbiq.gwt.tools.rpcrequest.browser.RpcRequestService#execute(org.tbiq.gwt.tools
+   * .rpcrequest.browser.RpcRequest)
+   */
+  @Override
+  public <T extends RpcResponse> T execute(RpcRequest<T> rpcRequest)
+    throws RpcRequestException
+  {
+    return null;
+  }
+}
